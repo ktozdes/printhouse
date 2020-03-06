1a5eb9493c1bb76fbe0d3f32073c49d6c8c0f6c0
Chyngyz Sydykov <chyngyz6@gmail.com>
2020-03-06 11:05:59 +0600

    api router fixed

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,29 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+Route::post('/login', 'Auth\LoginController@login')->middleware(['cors']);
+Route::post('/signback', 'Auth\LoginController@signback')->middleware(['cors']);
+Route::post('/register', 'Auth\LoginController@register')->middleware(['cors']);
+
+Route::get('/show', 'UserController@show')->middleware(['cors', 'token']);
+
+Route::get('/plate/list', 'PlateController@list')->middleware(['cors', 'token']);
+Route::post('/plate/store', 'PlateController@store')->middleware(['cors', 'token']);
+
+
+Route::post('/file/upload', 'FileController@upload')->middleware(['cors', 'token']);
+Route::get('/file/destroy', 'FileController@destroy')->middleware(['cors', 'token']);
+
+Route::get('/order/list', 'OrderController@list')->middleware(['cors', 'token']);
+Route::post('/order/store', 'OrderController@store')->middleware(['cors', 'token']);
+Route::get('/order/edit', 'OrderController@edit')->middleware(['cors', 'token']);
+Route::post('/order/update', 'OrderController@update')->middleware(['cors', 'token']);
+Route::post('/order/destroy', 'OrderController@destroy')->middleware(['cors', 'token']);
+
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
